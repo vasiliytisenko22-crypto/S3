89afaf3a906076d2ca8a091b5bda9979bc95671a
Serhii-Bohatkin <serhii.bohatkin.dev@gmail.com>
2026-03-27 13:30:46 +0200

    edit config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      // Використовуємо простіший збіг за префіксом
+      '/s3': {
+        target: 's3-railway-demo-production-26d6.up.railway.app',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
